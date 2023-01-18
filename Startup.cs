92f7ce656971af71e9019608cc186ff16ec02d20
andredias20 <andre.c.dias2000@gmail.com>
2023-01-18 18:22:17 -0300

    config-making_some_changes

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,41 @@
-﻿namespace ProductCatalogAPI
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalogAPI.Database;
+
+namespace ProductCatalogAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
 
-        public IConfiguration Configuration { get; set; }
 
         public Startup(IConfiguration configuration)
         {
-            Configuration= configuration;
+            Configuration = configuration;
         }
 
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<ProductContext>(opt =>
+              opt.UseSqlServer(Configuration.GetConnectionString("urlConnection")));
 
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
 
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.UseRouting();
+            app.UseEndpoints(map => { map.MapControllers(); });
+
+        }
     }
 }
